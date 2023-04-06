bd773cdd1161b828a127c684f6e002e32ad089df
Jose Luis De Miguel <joseluis.demiguel@vipera.com>
2023-04-06 20:39:21 +0200

    Cursos module first commit

---
+++ Cursos-SpringCloud/src/main/java/com/cloud/docker/k8s/cursos/CursosSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.docker.k8s.cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursosSpringCloudApplication.class, args);
+	}
+
+}
---
+++ Cursos-SpringCloud/src/test/java/com/cloud/docker/k8s/cursos/CursosSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.docker.k8s.cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosSpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
