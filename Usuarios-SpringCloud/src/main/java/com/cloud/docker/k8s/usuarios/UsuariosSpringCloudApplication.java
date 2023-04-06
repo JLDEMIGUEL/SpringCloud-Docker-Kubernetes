d19930172f867ed7f6c1077b424192338cf10301
Jose Luis De Miguel <joseluis.demiguel@vipera.com>
2023-04-06 18:38:00 +0200

    First commit

---
+++ Usuarios-SpringCloud/src/main/java/com/cloud/docker/k8s/usuarios/UsuariosSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.docker.k8s.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosSpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsuariosSpringCloudApplication.class, args);
+    }
+
+}
---
+++ Usuarios-SpringCloud/src/test/java/com/cloud/docker/k8s/usuarios/UsuariosSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.docker.k8s.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosSpringCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
