729a6fd0344bd1f37bc0558aacc26da8ea9dab7a
Jose Luis De Miguel <joseluis.demiguel@vipera.com>
2024-01-15 19:44:01 +0100

    Added msvc-auth and msvc-gateway to same repository

---
+++ msvc-auth/src/test/java/com/cloud/docker/k8s/msvc/auth/MsvcAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.docker.k8s.msvc.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
